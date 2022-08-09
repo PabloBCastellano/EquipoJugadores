dfab3be2d70cc46c1b5d6b4d498577881f2604c0
PabloBCastellano <pablo.branuelas@campusfp.es>
2022-08-09 09:25:28 +0200

    Create the Structure of trhe proyect
    Create the Structure of trhe proyect

---
+++ EquiposJugadores/database/factories/JugadoresFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class JugadoresFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            "id"=>$this->faker->id(),
+            "NamePlayer"=>$this->faker->name(),
+
+        ];
+    }
+}
